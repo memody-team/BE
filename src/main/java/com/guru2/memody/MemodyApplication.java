12ef4456f6070027d327914e43a07feabe7953f7
5y0ung4 <qwerrr504@naver.com>
2026-01-14 10:55:46 +0900

    init: 초기 세팅

---
+++ src/main/java/com/guru2/memody/MemodyApplication.java
@@ -0,0 +1,13 @@
+package com.guru2.memody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guru2/memody/MemodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru2.memody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
